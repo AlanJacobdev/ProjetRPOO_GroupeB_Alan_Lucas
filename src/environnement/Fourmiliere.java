1d0818a6286da7bde3ee5e42fc55832b2ca88351
Alan-Raich <33010105+Alan-Raich@users.noreply.github.com>
2020-12-01 10:51:22 +0100

    patch fourmi

--- src/environnement/Fourmiliere.java
+++ src/environnement/Fourmiliere.java
@@ -3,16 +3,16 @@ package environnement;
 import fourmis.Fourmis;
 import java.util.ArrayList;
 import java.util.List;
-
+import role.Reine;
 
 public class Fourmiliere {
 
   List<Fourmis> lesFourmis;
+  Fourmis reine;
 
-  public Fourmiliere() {
+  public Fourmiliere(Reine reine) {
     this.lesFourmis = new ArrayList<>();
   }
-
 
 
 }
