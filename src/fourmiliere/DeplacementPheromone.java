84a0f0c6ce80eb77ca991e032c057752c5a2fde2
Alan-Raich <33010105+Alan-Raich@users.noreply.github.com>
2020-12-04 19:40:09 +0100

    modifier class deplacement

--- src/fourmiliere/DeplacementPheromone.java
+++ src/fourmiliere/DeplacementPheromone.java
@@ -21,7 +21,7 @@ public class DeplacementPheromone {
    * @param f Fourmi adulte qui souhaite se déplacer.
    * @return
    *         <p>
-   *         La liste des poucentage : <br/>
+   *         La liste des poucentage (case): <br/>
    *         0 Vers le haut <br/>
    *         1 Vers le bas<br/>
    *         2 Vers la droite <br/>
@@ -29,105 +29,87 @@ public class DeplacementPheromone {
    *         </p>
    */
   public List<Integer> calculPourcentageDeplacement(Adulte f) {
-    List<Integer> liste = new ArrayList<Integer>();
     Boolean pheromoneDroit = false;
     Boolean pheromoneGauche = false;
     Boolean pheromoneHaut = false;
     Boolean pheromoneBas = false;
     int nombrePheromone = 0;
     Point coordonneesFourmi = f.getRepresentationGraphique().getCoordonneesActuelle();
-    Pheromone haut = f.getFourmis().getFourmiliere().getLeTerrain()
-        .getUnPheromone(coordonneesFourmi.x, coordonneesFourmi.y + 5);
     Pheromone bas = f.getFourmis().getFourmiliere().getLeTerrain()
         .getUnPheromone(coordonneesFourmi.x, coordonneesFourmi.y - 5);
-    Pheromone droite = f.getFourmis().getFourmiliere().getLeTerrain()
-        .getUnPheromone(coordonneesFourmi.x + 5, coordonneesFourmi.y);
-    Pheromone gauche = f.getFourmis().getFourmiliere().getLeTerrain()
-        .getUnPheromone(coordonneesFourmi.x - 5, coordonneesFourmi.y);
-    int intensiteHaut = haut.getIntensitePheromone();
+    
     int intensiteBas = bas.getIntensitePheromone();
-    int intensiteDroit = droite.getIntensitePheromone();
-    int intensiteGauche = gauche.getIntensitePheromone();
     if (intensiteBas > 0) {
       pheromoneBas = true;
       nombrePheromone++;
     }
+    Pheromone haut = f.getFourmis().getFourmiliere().getLeTerrain()
+        .getUnPheromone(coordonneesFourmi.x, coordonneesFourmi.y + 5);
+    int intensiteHaut = haut.getIntensitePheromone();
     if (intensiteHaut > 0) {
       pheromoneHaut = true;
       nombrePheromone++;
     }
+    Pheromone droite = f.getFourmis().getFourmiliere().getLeTerrain()
+        .getUnPheromone(coordonneesFourmi.x + 5, coordonneesFourmi.y);
+    int intensiteDroit = droite.getIntensitePheromone();
     if (intensiteDroit > 0) {
       pheromoneDroit = true;
       nombrePheromone++;
     }
+    Pheromone gauche = f.getFourmis().getFourmiliere().getLeTerrain()
+        .getUnPheromone(coordonneesFourmi.x - 5, coordonneesFourmi.y);
+    int intensiteGauche = gauche.getIntensitePheromone();
     if (intensiteGauche > 0) {
       pheromoneGauche = true;
       nombrePheromone++;
     }
+    return this.creerListeDeplacement(nombrePheromone, pheromoneHaut, pheromoneBas, pheromoneDroit,
+        pheromoneGauche);
+  }
+
+  /**
+   * Créer la liste qui regroupe les pourcentages de déplacement en fonction du nombre de phéromones
+   * et leur présence autour de la fourmi.
+   * 
+   * @param nombrePheromone Nombre de phéromones entourant la fourmis (max 4)
+   * @param pheromoneHaut   Présence de phéromone sur la cas du haut ou non
+   * @param pheromoneBas    Présence de phéromone sur la cas du bas ou non
+   * @param pheromoneDroit  Présence de phéromone sur la cas du droit ou non
+   * @param pheromoneGauche Présence de phéromone sur la cas du gauche ou non
+   * @return Liste contenant les pourcentages de déplacement tel que [Haut,Bas,Droite,Gauche]
+   */
+  public List<Integer> creerListeDeplacement(int nombrePheromone, Boolean pheromoneHaut,
+      Boolean pheromoneBas, Boolean pheromoneDroit, Boolean pheromoneGauche) {
+    List<Integer> liste = new ArrayList<Integer>();
 
     if (nombrePheromone == 1) {
-      if (pheromoneHaut) {
-        liste.add(40);
-      } else {
-        liste.add(20);
-      }
-      if (pheromoneBas) {
-        liste.add(40);
-      } else {
-        liste.add(20);
-      }
-      if (pheromoneDroit) {
-        liste.add(40);
-      } else {
-        liste.add(20);
-      }
-      if (pheromoneGauche) {
-        liste.add(40);
-      } else {
-        liste.add(20);
-      }
+      if (pheromoneHaut ? liste.add(40) : liste.add(20))
+        ;
+      if (pheromoneBas ? liste.add(40) : liste.add(20))
+        ;
+      if (pheromoneDroit ? liste.add(40) : liste.add(20))
+        ;
+      if (pheromoneGauche ? liste.add(40) : liste.add(20))
+        ;
     } else if (nombrePheromone == 2) {
-      if (pheromoneHaut) {
-        liste.add(35);
-      } else {
-        liste.add(15);
-      }
-      if (pheromoneBas) {
-        liste.add(35);
-      } else {
-        liste.add(15);
-      }
-      if (pheromoneDroit) {
-        liste.add(35);
-      } else {
-        liste.add(15);
-      }
-      if (pheromoneGauche) {
-        liste.add(35);
-      } else {
-        liste.add(15);
-      }
+      if (pheromoneHaut ? liste.add(35) : liste.add(15))
+        ;
+      if (pheromoneBas ? liste.add(35) : liste.add(15))
+        ;
+      if (pheromoneDroit ? liste.add(35) : liste.add(15))
+        ;
+      if (pheromoneGauche ? liste.add(35) : liste.add(15))
+        ;
     } else if (nombrePheromone == 3) {
-      if (pheromoneHaut) {
-        liste.add(30);
-      } else {
-        liste.add(10);
-      }
-      if (pheromoneBas) {
-        liste.add(30);
-      } else {
-        liste.add(10);
-      }
-      if (pheromoneDroit) {
-        liste.add(30);
-      } else {
-        liste.add(10);
-      }
-      if (pheromoneGauche) {
-        liste.add(30);
-      } else {
-        liste.add(10);
-      }
+      if (pheromoneHaut ? liste.add(30) : liste.add(10))
+        ;
+      if (pheromoneBas ? liste.add(30) : liste.add(10))
+        ;
+      if (pheromoneDroit ? liste.add(30) : liste.add(10))
+        ;
+      if (pheromoneGauche ? liste.add(30) : liste.add(10))
+        ;
     } else {
       liste.add(25);
       liste.add(25);
@@ -136,5 +118,4 @@ public class DeplacementPheromone {
     }
     return liste;
   }
-
 }
